33b8c7c558673389468cd7b35542e04d602ac7ba
Briankip63 <kipkemboi.brian@yahoo.com>
2021-03-11 18:29:01 +0300

    Header Complete

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from './components/Navbar';
+import Header from "./components/Header";
+
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Navbar />
-    </div>
+      <Header />
+    </>
   );
 }
 
